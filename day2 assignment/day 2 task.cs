a0866ca08149e75edc0dae33e49df62c4d34460b
tejaswini200029 <2018tejaswini@gmail.com>
2024-07-10 15:49:35 +0530

    Add files via upload

---
+++ day2 assignment/day 2 task.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter your name");
+String Name = Console.ReadLine();
+Console.WriteLine("enter your age");
+int Age = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Your name" + Name);
+Console.WriteLine("your age" +Age);
+
